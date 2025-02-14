95c1233fc273e996f185f1b510cdacc184803d73
Dam2204 <soyoung4576@gmail.com>
2025-02-14 18:37:32 +0900

    🎉 add: logger

--- src/server.js
+++ src/server.js
@@ -4,11 +4,14 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import UsersRouter from './routes/usersRouter.js';
 import { PORT } from './constants/env.js';
+import LogMiddleware from './middlewares/logMiddleware.js';
 
 const app = express();
 
+app.use(LogMiddleware);
 app.use(express.json());
 app.use(cookieParser());
+
 app.use('/api', [UsersRouter]);
 
 app.listen(PORT, () => {
