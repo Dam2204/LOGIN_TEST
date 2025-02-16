e570378d6fb30f5417fed717e9a34b03ab811478
Dam2204 <soyoung4576@gmail.com>
2025-02-16 14:15:35 +0900

    ✨ update: response renewal

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -8,7 +8,7 @@ export class UsersController {
       const { username, password, nickname } = req.body;
       const user = await this.usersService.signUp(username, password, nickname);
 
-      return res.status(200).json(user);
+      return res.status(user.status).json(user.message);
     } catch (err) {
       next(err);
     }
@@ -20,12 +20,15 @@ export class UsersController {
 
       const user = await this.usersService.signIn(username, password);
 
+      const accessToken = user.message.accessToken;
+      const refreshToken = user.message.refreshToken;
+
       // 쿠키에 토큰 전달
-      res.cookie('accessToken', `Bearer ${user.accessToken}`);
-      res.cookie('refreshToken', `Bearer ${user.refreshToken}`);
+      res.cookie('accessToken', `Bearer ${accessToken}`);
+      res.cookie('refreshToken', `Bearer ${refreshToken}`);
 
       // 사용자에게 반환되는 데이터
-      return res.status(200).json({ token: user.accessToken });
+      return res.status(user.status).json(user.message);
     } catch (err) {
       next(err);
     }
--- src/services/usersService.js
+++ src/services/usersService.js
@@ -16,26 +16,41 @@ export class UsersService {
 
       // 이미 존재하는 사용자일 경우
       if (isExistUser) {
-        throw new CustomError('이미 존재하는 username 입니다.', 400);
+        const errorMessage = {
+          status: 400,
+          message: '이미 존재하는 username 입니다.',
+        };
+        return errorMessage;
       }
 
       // username 조건에 벗어나는 경우
       if (!isValidUsername(username)) {
-        throw new CustomError('username은 영어 소문자 1~30 글자만 입력 가능합니다.', 400);
+        const errorMessage = {
+          status: 400,
+          message: 'username은 영어 소문자 1~30 글자만 입력 가능합니다.',
+        };
+        return errorMessage;
       }
 
       // password 조건에 벗어나는 경우
       if (!isValidPassword(password)) {
-        throw new CustomError(
-          'password는 최소 8자리 이상 영문 대소문자, 숫자, 특수문자가 각각 1개 이상 입력해주세요.',
-          400,
-        );
+        const errorMessage = {
+          status: 400,
+          message:
+            'password는 최소 8자리 이상 영문 대소문자, 숫자, 특수문자가 각각 1개 이상 입력해주세요.',
+        };
+        return errorMessage;
       }
 
       // 신규 사용자 등록
       const createUser = await this.usersRepository.createUser(username, password, nickname);
 
-      return createUser;
+      const successMessage = {
+        status: 200,
+        message: createUser,
+      };
+
+      return successMessage;
     } catch (err) {
       console.error(err);
     }
@@ -48,12 +63,20 @@ export class UsersService {
 
       // username 검증
       if (!user) {
-        throw new CustomError('존재하지 않는 사용자입니다.', 401);
+        const errorMessage = {
+          status: 401,
+          message: '존재하지 않는 사용자입니다.',
+        };
+        return errorMessage;
       }
 
       // password 검증
       if (!(await bcrypt.compare(password, user.password))) {
-        throw new CustomError('비밀번호가 일치하지 않습니다.', 401);
+        const errorMessage = {
+          status: 401,
+          message: '비밀번호가 일치하지 않습니다.',
+        };
+        return errorMessage;
       }
 
       // jwt 토큰 발급
@@ -64,6 +87,13 @@ export class UsersService {
         accessToken: accessToken,
         refreshToken: refreshToken,
       };
+
+      const successMessage = {
+        status: 200,
+        message: token,
+      };
+
+      return successMessage;
 
       // 사용자에게 반환되는 데이터
       return token;
